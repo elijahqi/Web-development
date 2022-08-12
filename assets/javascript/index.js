0fd236cb7d0def562cd65f96a955f8a673c178c0
elijahqi <89861285+elijahqi@users.noreply.github.com>
2022-08-12 10:57:51 -0400

    Add files via upload

---
+++ assets/javascript/index.js
@@ -0,0 +1,17 @@
+var dnumber1=Math.floor(Math.random()*6)+1;
+var dnumber2=Math.floor(Math.random()*6)+1;
+var img1="assets/images/dice"+dnumber1+".png";
+var img2="assets/images/dice"+dnumber2+".png";
+
+var diceimg1=document.querySelector(".img1").setAttribute("src",img1);
+var diceimg2=document.querySelector(".img2").setAttribute("src",img2);
+
+
+var titletxt=document.querySelector("h1");
+if (dnumber1===dnumber2){
+  titletxt.textContent="Draw!";
+}else if (dnumber1>dnumber2){
+  titletxt.textContent="🚩 Player 1 Wins!";
+}else{
+  titletxt.textContent="Player 2 Wins! 🚩";
+}
